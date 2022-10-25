842fd2f9698810fb5b879bd93bd96320a86d227d
vinceH <vinceHuyghe@users.noreply.github.com>
2022-10-25 19:43:14 +0000

    node

--- src/ros_osv/ros_osv.py
+++ src/ros_osv/ros_osv.py
@@ -1,8 +1,9 @@
 import rospy
 import tf
-from tf.msg import tfMessage
-from geometry_msgs.msg import Transform, TransformStamped, Vector3, Quaternion
-from typing import List
+import tf2_ros
+from tf2_msgs.msg import TFMessage
+from std_msgs.msg import Header
+from geometry_msgs.msg import TransformStamped
 
 
 from py_openshowvar import OpenShowVar
@@ -14,11 +15,11 @@ class OsvNode:
         rospy.init_node('osv')
         self.name = rospy.get_name()
         # init osv connection
-        self.client = OpenShowVar(ip=ip, port=port)
-        self.tf_br = tf.TransformBroadcaster()
-        rospy.loginfo(
-            f'{self.name} node started, KVP CONNECTED {self.client.can_connect}'
-        )
+        # self.client = OpenShowVar(ip=ip, port=port)
+        self.tf_br = tf2_ros.TransformBroadcaster()
+        # rospy.loginfo(
+        #     f'{self.name} node started, KVP CONNECTED {self.client.can_connect}'
+        # )
 
     def read_var(self, var: str) -> str:
         # return self.client.read("$POS_ACT", debug=False)
@@ -30,22 +31,31 @@ class OsvNode:
 
             var = self.read_var('$POS_ACT')
 
-        var = var.replace(',', '').split(' ')
-        pos = [var[2], var[4], var[6], var[8], var[10], var[12], var[14]]
+            var = var.replace(',', '').split(' ')
+            pos = list(
+                map(float, [var[2], var[4], var[6], var[8], var[10], var[12], var[14]])
+            )
+            
+            quat = tf.transformations.quaternion_from_euler(pos[4], pos[5], pos[6])
 
-        quat = transformations.quaternion_from_euler(
-            float(pos[4]), float(pos[5]), float(pos[6])
-        )
+            tfs = TransformStamped()
 
-        tf_msg = Transform(
-            Vector3(x=pos[0], y=pos[1], z=pos[2]),
-            Quaternion(
-                x=quat[1],
-            ),
-        )
+            tfs.header.stamp = rospy.Time.now()
+            tfs.header.frame_id = 'base_link'
+            tfs.child_frame_id = 'tcp'
+            tfs.transform.translation.x = pos[0]
+            tfs.transform.translation.y = pos[1]
+            tfs.transform.translation.z = pos[2]
+            tfs.transform.rotation.x = quat[0]
+            tfs.transform.rotation.y = quat[1]
+            tfs.transform.rotation.z = quat[2]
+            tfs.transform.rotation.w = quat[3]
+
+            self.tf_br.sendTransform(tfs)
 
 
 if __name__ == '__main__':
 
     osv = OsvNode("192.168.1.100", 7000)
-    print(osv.read_var("$POS_ACT"))
+    osv.broadcast_tf_from_var()
+    
