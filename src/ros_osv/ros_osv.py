e3bb11875298b5b3ffb7ee434a5ed667892c2643
vinceHuyghe <vinceHuyghe@users.noreply.github.com>
2022-10-25 17:05:39 +0000

    osv

--- src/ros_osv/ros_osv.py
+++ src/ros_osv/ros_osv.py
@@ -1,40 +1,51 @@
 import rospy
 import tf
 from tf.msg import tfMessage
+from geometry_msgs.msg import Transform, TransformStamped, Vector3, Quaternion
+from typing import List
 
 
 from py_openshowvar import OpenShowVar
 
 
-class OsvNode():
-
+class OsvNode:
     def __init__(self, ip: str, port: int) -> None:
+
         rospy.init_node('osv')
         self.name = rospy.get_name()
-        # init connection
+        # init osv connection
         self.client = OpenShowVar(ip=ip, port=port)
-        # self.tf_br = tf.TransformBroadcaster()
+        self.tf_br = tf.TransformBroadcaster()
         rospy.loginfo(
-            f'{self.name} node started, KVP CONNECTED {self.client.can_connect}')
-        # self.tfmsg = tfMessage()
-        print(self.client.can_connect)
+            f'{self.name} node started, KVP CONNECTED {self.client.can_connect}'
+        )
 
     def read_var(self, var: str) -> str:
-        return self.client.read(var)
+        # return self.client.read("$POS_ACT", debug=False)
+        return '{E6POS: X 12.84936, Y -394.9386, Z 368.6748, A 17.71607, B 89.99682, C -1.162400, S 18, T 10, E1 90.00000, E2 0.0, E3 0.0, E4 0.0, E5 0.0, E6 0.0}'
 
-    # def resp_to_tf(self, resp: str) -> tfMessage:
+    def broadcast_tf_from_var(self):
 
-    #     self.tfmsg = resp
+        while not rospy.is_shutdown():
 
-    #     return self.tfmsg
+            var = self.read_var('$POS_ACT')
 
-    # def run(self):
-    #     while not rospy.is_shutdown:
-    #         self.tf_br.sendTransform(
-    #             self.resp_to_tf(self.read_var('$POS_ACT')))
+        var = var.replace(',', '').split(' ')
+        pos = [var[2], var[4], var[6], var[8], var[10], var[12], var[14]]
+
+        quat = transformations.quaternion_from_euler(
+            float(pos[4]), float(pos[5]), float(pos[6])
+        )
+
+        tf_msg = Transform(
+            Vector3(x=pos[0], y=pos[1], z=pos[2]),
+            Quaternion(
+                x=quat[1],
+            ),
+        )
 
 
 if __name__ == '__main__':
 
-    osv = OsvNode('192.168.1.100', 7001)
-    print(osv.read_var('$POS_ACT'))
+    osv = OsvNode("192.168.1.100", 7000)
+    print(osv.read_var("$POS_ACT"))
--- src/ros_osv/test.py
+++ src/ros_osv/test.py
@@ -3,31 +3,33 @@ from tf import transformations
 from tf.msg import tfMessage
 from geometry_msgs.msg import Transform
 
-client = OpenShowVar(ip='192.168.1.100', port=7000)
+# client = OpenShowVar(ip='192.168.1.100', port=7000)
 
-if client.can_connect:
-    var = client.read('$POS_ACT', debug=False)
-    var = var.decode()
-    var = var.split()
-    pos = [var[2].strip(','),var[4].strip(','),var[6].strip(','),var[8].strip(','),var[10].strip(','),var[12].strip(','),var[14].strip(',')]
-    
-    tf_msg = Transform()
-    tf_msg.translation.x = pos[0]
-    tf_msg.translation.y = pos[1]
-    tf_msg.translation.z = pos[2]
-    
-    quat = transformations.quaternion_from_euler(
-        float(pos[4]),
-        float(pos[5]),
-        float(pos[6]))
-    
-    tf_msg.rotation.x = quat[0]
-    tf_msg.rotation.y = quat[1]
-    tf_msg.rotation.z = quat[2]
-    tf_msg.rotation.w = quat[3]
-    
-    print(tf_msg)
-else:
-    print('unable to connect')
+# if client.can_connect:
+
+# var = client.read('$POS_ACT', debug=False)
+var = '{E6POS: X 12.84936, Y -394.9386, Z 368.6748, A 17.71607, B 89.99682, C -1.162400, S 18, T 10, E1 90.00000, E2 0.0, E3 0.0, E4 0.0, E5 0.0, E6 0.0}'
+# var = var.decode()
+var = var.replace(',', '').split(' ')
+pos = [var[2], var[4], var[6], var[8], var[10], var[12], var[14]]
+print(pos)
+
+# tf_msg = Transform()
+# tf_msg.translation.x = pos[0]
+# tf_msg.translation.y = pos[1]
+# tf_msg.translation.z = pos[2]
+
+# quat = transformations.quaternion_from_euler(
+#     float(pos[4]), float(pos[5]), float(pos[6])
+# )
+
+# tf_msg.rotation.x = quat[0]
+# tf_msg.rotation.y = quat[1]
+# tf_msg.rotation.z = quat[2]
+# tf_msg.rotation.w = quat[3]
+
+# print(tf_msg)
+# else:
+#     print('unable to connect')
 
 # {E6POS: X 12.84936, Y -394.9386, Z 368.6748, A 17.71607, B 89.99682, C -1.162400, S 18, T 10, E1 90.00000, E2 0.0, E3 0.0, E4 0.0, E5 0.0, E6 0.0}
