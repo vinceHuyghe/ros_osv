799d86f27ab3a86e94aefeb5fd474d55d070ae74
vinceH <vinceHuyghe@users.noreply.github.com>
2022-10-25 20:21:51 +0000

    node

--- scripts/osv_broadcast.py
+++ scripts/osv_broadcast.py
@@ -25,37 +25,43 @@ class OsvNode:
         # return self.client.read("$POS_ACT", debug=False)
         return '{E6POS: X 12.84936, Y -394.9386, Z 368.6748, A 17.71607, B 89.99682, C -1.162400, S 18, T 10, E1 90.00000, E2 0.0, E3 0.0, E4 0.0, E5 0.0, E6 0.0}'
 
-    def broadcast_tf_from_var(self):
+    def broadcast(self):
 
         while not rospy.is_shutdown():
 
+            # TODD check for car only version
+
             var = self.read_var('$POS_ACT')
 
-            var = var.replace(',', '').split(' ')
-            pos = list(
-                map(float, [var[2], var[4], var[6], var[8], var[10], var[12], var[14]])
-            )
-            
-            quat = tf.transformations.quaternion_from_euler(pos[4], pos[5], pos[6])
+            self.tf_br.sendTransform(OsvNode.pos_act_to_tf(var))
 
-            tfs = TransformStamped()
+    @staticmethod
+    def pos_act_to_tf(var: str) -> TransformStamped:
 
-            tfs.header.stamp = rospy.Time.now()
-            tfs.header.frame_id = 'base_link'
-            tfs.child_frame_id = 'tcp'
-            tfs.transform.translation.x = pos[0]
-            tfs.transform.translation.y = pos[1]
-            tfs.transform.translation.z = pos[2]
-            tfs.transform.rotation.x = quat[0]
-            tfs.transform.rotation.y = quat[1]
-            tfs.transform.rotation.z = quat[2]
-            tfs.transform.rotation.w = quat[3]
+        var = var.replace(',', '').split(' ')
+        pos = list(
+            map(float, [var[2], var[4], var[6], var[8], var[10], var[12], var[14]])
+        )
 
-            self.tf_br.sendTransform(tfs)
+        quat = tf.transformations.quaternion_from_euler(pos[4], pos[5], pos[6])
+
+        tfs = TransformStamped()
+
+        tfs.header.stamp = rospy.Time.now()
+        tfs.header.frame_id = 'base_link'
+        tfs.child_frame_id = 'tcp'
+        tfs.transform.translation.x = pos[0]
+        tfs.transform.translation.y = pos[1]
+        tfs.transform.translation.z = pos[2]
+        tfs.transform.rotation.x = quat[0]
+        tfs.transform.rotation.y = quat[1]
+        tfs.transform.rotation.z = quat[2]
+        tfs.transform.rotation.w = quat[3]
+
+        return tfs
 
 
 if __name__ == '__main__':
 
     osv = OsvNode("192.168.1.100", 7000)
     osv.broadcast_tf_from_var()
-    
