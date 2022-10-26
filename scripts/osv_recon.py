0331dc33e30d11fa24ea389e1cea3a9a8d1b1182
vinceHuyghe <vinceHuyghe@users.noreply.github.com>
2022-10-26 16:28:06 +0000

    recon

--- scripts/osv_recon.py
+++ scripts/osv_recon.py
@@ -3,41 +3,41 @@ import tf.transformations
 import tf2_ros
 from tf2_msgs.msg import TFMessage
 from std_msgs.msg import Header
-from geometry_msgs.msg import TransformStamped
+from geometry_msgs.msg import TransformStamped, Vector3
 
-# from industrial_reconstruction_msgs.msg import NormalFilterParams
-# from industrial_reconstruction_msgs.srv import (
-#     StartReconstruction,
-#     StartReconstructionRequest,
-#     StopReconstruction,
-#     StopReconstructionRequest,
-# )
+from industrial_reconstruction_msgs.msg import NormalFilterParams
+from industrial_reconstruction_msgs.srv import (
+    StartReconstruction,
+    StartReconstructionRequest,
+    StopReconstruction,
+    StopReconstructionRequest,
+)
 
 
 from ros_osv.py_openshowvar import OpenShowVar
 import math
 
-# # reconstruction parameters
-# start_srv_req = StartReconstructionRequest()
-# start_srv_req.tracking_frame = "tool0"
-# start_srv_req.relative_frame = "base_link"
-# start_srv_req.translation_distance = 0.0
-# start_srv_req.rotational_distance = 0.0
-# start_srv_req.live = True
-# start_srv_req.tsdf_params.voxel_length = 0.02
-# start_srv_req.tsdf_params.sdf_trunc = 0.04
-# start_srv_req.tsdf_params.min_box_values = Vector3(x=0.0, y=0.0, z=0.0)
-# start_srv_req.tsdf_params.max_box_values = Vector3(x=0.0, y=0.0, z=0.0)
-# start_srv_req.rgbd_params.depth_scale = 1000
-# start_srv_req.rgbd_params.depth_trunc = 0.5
-# start_srv_req.rgbd_params.convert_rgb_to_intensity = False
+# reconstruction parameters
+start_srv_req = StartReconstructionRequest()
+start_srv_req.tracking_frame = "tool0"
+start_srv_req.relative_frame = "base_link"
+start_srv_req.translation_distance = 0.0
+start_srv_req.rotational_distance = 0.0
+start_srv_req.live = True
+start_srv_req.tsdf_params.voxel_length = 0.02
+start_srv_req.tsdf_params.sdf_trunc = 0.04
+start_srv_req.tsdf_params.min_box_values = Vector3(x=0.0, y=0.0, z=0.0)
+start_srv_req.tsdf_params.max_box_values = Vector3(x=0.0, y=0.0, z=0.0)
+start_srv_req.rgbd_params.depth_scale = 1000
+start_srv_req.rgbd_params.depth_trunc = 0.5
+start_srv_req.rgbd_params.convert_rgb_to_intensity = False
 
-# stop_srv_req = StopReconstructionRequest()
-# # stop_srv_req.archive_directory = '/dev_ws/src.reconstruction/'
-# stop_srv_req.mesh_filepath = '/home/v/test.ply'
-# # stop_srv_req.normal_filters = [NormalFilterParams(
-# #                     normal_direction=Vector3(x=0.0, y=0.0, z=1.0), angle=90)]
-# # stop_srv_req.min_num_faces = 1000
+stop_srv_req = StopReconstructionRequest()
+# stop_srv_req.archive_directory = '/dev_ws/src.reconstruction/'
+stop_srv_req.mesh_filepath = '/home/v/test.ply'
+# stop_srv_req.normal_filters = [NormalFilterParams(
+#                     normal_direction=Vector3(x=0.0, y=0.0, z=1.0), angle=90)]
+# stop_srv_req.min_num_faces = 1000
 
 digital_output_trigger = 25
 
@@ -53,12 +53,12 @@ class OsvRecon:
         rospy.loginfo(
             f'{self.name} node started, KVP CONNECTED {self.client.can_connect}'
         )
-        # rospy.wait_for_service('/start_reconstruction')
-        # rospy.loginfo('robot program: waiting for /start_reconstruction srv')
-        # self.start_recon = rospy.ServiceProxy(
-        #     '/start_reconstruction', StartReconstruction
-        # )
-        # self.stop_recon = rospy.ServiceProxy('/stop_reconstruction', StopReconstruction)
+        rospy.wait_for_service('/start_reconstruction')
+        rospy.loginfo('robot program: waiting for /start_reconstruction srv')
+        self.start_recon = rospy.ServiceProxy(
+            '/start_reconstruction', StartReconstruction
+        )
+        self.stop_recon = rospy.ServiceProxy('/stop_reconstruction', StopReconstruction)
 
     def read_and_broadcast(self):
 
@@ -76,23 +76,23 @@ class OsvRecon:
                 
             print(output_state)
 
-            # if output_state:
-            #     # Start reconstruction with service srv_req
-            #     resp = self.start_recon(start_srv_req)
-            #     if resp:
-            #         rospy.loginfo("robot program: reconstruction started successfully")
-            #         recon_started = True
-            #     else:
-            #         rospy.loginfo("robot program: failed to start reconstruction")
+            if output_state:
+                # Start reconstruction with service srv_req
+                resp = self.start_recon(start_srv_req)
+                if resp:
+                    rospy.loginfo("robot program: reconstruction started successfully")
+                    recon_started = True
+                else:
+                    rospy.loginfo("robot program: failed to start reconstruction")
 
-            # if not output_state and recon_started:
-            #     # Stop reconstruction with service srv_req
-            #     resp = self.stop_recon(stop_srv_req)
+            if not output_state and recon_started:
+                # Stop reconstruction with service srv_req
+                resp = self.stop_recon(stop_srv_req)
 
-            #     if resp:
-            #         rospy.loginfo("robot program: reconstruction stopped successfully")
-            #     else:
-            #         rospy.loginfo("robot program: failed to stop reconstruction")
+                if resp:
+                    rospy.loginfo("robot program: reconstruction stopped successfully")
+                else:
+                    rospy.loginfo("robot program: failed to stop reconstruction")
 
             # Read tcp position relative to base frame
             var = self.client.read('$POS_ACT', debug=False)
