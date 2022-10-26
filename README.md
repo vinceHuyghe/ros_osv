af6624efdbaf62bd9dc441939cccfcbd55776d0c
vinceHuyghe <vinceHuyghe@users.noreply.github.com>
2022-10-26 15:39:14 +0000

    pos_act and out tested

--- README.md
+++ README.md
@@ -11,7 +11,8 @@ ros node implementing pyopenshowvar
     -[py_openshowvar](https://github.com/linuxsand/py_openshowvar)
     -[kuka var proxy msg format](https://github.com/akselov/kukavarproxy-msg-format/blob/master/python/kukavarproxy.py)  
     -[kuka kvp hw interface](https://github.com/itk-thrivaldi/kuka_kvp_hw_interface)  
-    -[kuka ros open communication](https://github.com/itk-thrivaldi/kuka_kvp_hw_interface)  
+    -[kuka ros open communication](https://github.com/itk-thrivaldi/kuka_kvp_hw_interface)
+    -[variabels](https://github.com/ImtsSrl/openshowvar/tree/master/resources)
 
 - Papers  
     -[Controlling Kuka Industrial Robots: Flexible Communication Interface JOpenShowVar](http://filipposanfilippo.inspitivity.com/publications/controlling-kuka-industrial-robots-flexible-communication-interface-jopenshowvar.pdf)  
---
+++ scripts/osv_recon.py
@@ -0,0 +1,100 @@
+import rospy
+import tf.transformations
+import tf2_ros
+from tf2_msgs.msg import TFMessage
+from std_msgs.msg import Header
+from geometry_msgs.msg import TransformStamped
+
+from industrial_reconstruction_msgs.msg import NormalFilterParams
+from industrial_reconstruction_msgs.srv import (StartReconstruction,
+                                                StartReconstructionRequest,
+                                                StopReconstruction,
+                                                StopReconstructionRequest)
+
+
+from ros_osv.py_openshowvar import OpenShowVar
+import math
+
+# reconstruction parameters
+start_srv_req = StartReconstructionRequest()
+start_srv_req.tracking_frame = "tool0"
+start_srv_req.relative_frame = "base_link"
+start_srv_req.translation_distance = 0.0
+start_srv_req.rotational_distance = 0.0
+start_srv_req.live = True
+start_srv_req.tsdf_params.voxel_length = 0.02
+start_srv_req.tsdf_params.sdf_trunc = 0.04
+start_srv_req.tsdf_params.min_box_values = Vector3(x=0.0, y=0.0, z=0.0)
+start_srv_req.tsdf_params.max_box_values = Vector3(x=0.0, y=0.0, z=0.0)
+start_srv_req.rgbd_params.depth_scale = 1000
+start_srv_req.rgbd_params.depth_trunc = 0.5
+start_srv_req.rgbd_params.convert_rgb_to_intensity = False
+
+stop_srv_req = StopReconstructionRequest()
+# stop_srv_req.archive_directory = '/dev_ws/src.reconstruction/'
+stop_srv_req.mesh_filepath = "/home/v/test.ply"
+# stop_srv_req.normal_filters = [NormalFilterParams(
+#                     normal_direction=Vector3(x=0.0, y=0.0, z=1.0), angle=90)]
+# stop_srv_req.min_num_faces = 1000
+
+
+class OsvRecon:
+    def __init__(self, ip: str, port: int) -> None:
+
+        rospy.init_node('osv')
+        self.name = rospy.get_name()
+        # init osv connection
+        self.client = OpenShowVar(ip=ip, port=port)
+        self.tf_br = tf2_ros.TransformBroadcaster()
+        rospy.loginfo(
+            f'{self.name} node started, KVP CONNECTED {self.client.can_connect}'
+        )
+        rospy.wait_for_service('/start_reconstruction')
+        rospy.loginfo('robot program: waiting for /start_reconstruction srv')
+        self.start_recon = rospy.ServiceProxy('/start_reconstruction', StartReconstruction)
+        self.stop_recon = rospy.ServiceProxy('/stop_reconstruction', StopReconstruction)
+
+    def read_and_broadcast(self):
+
+        while not rospy.is_shutdown():
+            
+            # Read digital output
+            var = self.client.read('$OUT[11]', debug=False)
+            output_state = var.decode()
+
+            # Read tcp position relative to base frame
+            var = self.client.read('$POS_ACT', debug=False)
+            self.tf_br.sendTransform(OsvRecon.pos_act_to_tf(var.decode()))
+    
+    
+    @staticmethod
+    def pos_act_to_tf(var: str) -> TransformStamped:
+
+        # POS_ACT is relative to base frame
+        var = var.replace(',', '').split(' ')
+        pos = list(map(float, [var[2], var[4], var[6], var[8], var[10], var[12]]))
+
+        quat = tf.transformations.quaternion_from_euler(
+            math.radians(pos[3]), math.radians(pos[4]), math.radians(pos[5]), 'rxyz'
+        )
+
+        tfs = TransformStamped()
+
+        tfs.header.stamp = rospy.Time.now()
+        tfs.header.frame_id = 'base_link'
+        tfs.child_frame_id = 'tcp'
+        tfs.transform.translation.x = pos[0] / 1000
+        tfs.transform.translation.y = pos[1] / 1000
+        tfs.transform.translation.z = pos[2] / 1000
+        tfs.transform.rotation.x = quat[0]
+        tfs.transform.rotation.y = quat[1]
+        tfs.transform.rotation.z = quat[2]
+        tfs.transform.rotation.w = quat[3]
+
+        return tfs
+
+
+if __name__ == '__main__':
+
+    osv = OsvRecon("192.168.1.100", 7000)
+    osv.read_and_broadcast()
